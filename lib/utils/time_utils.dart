c488a553e44a8932e3a2e600cdccf1d5c1429164
Kyrylo Stepanov <kstepanov@quantum-systems.com>
2026-04-07 15:18:41 +0300

    remove useless stopwatch precision

--- lib/utils/time_utils.dart
+++ lib/utils/time_utils.dart
@@ -4,8 +4,8 @@ import 'package:gdf_hema_timer/modules/timer/storage.dart';
 String formatTime(Duration d) {
   final minutes = d.inMinutes.remainder(60).toString().padLeft(2, '0');
   final seconds = d.inSeconds.remainder(60).toString().padLeft(2, '0');
-  final millis = d.inMilliseconds.remainder(1000).toString().padLeft(3, '0');
-  return "$minutes:$seconds.$millis";
+  final centis = (d.inMilliseconds.remainder(1000) ~/ 10).toString().padLeft(2, '0');
+  return "$minutes:$seconds.$centis";
 }
 
 Future<Duration?> showTimeSelectDialog(BuildContext context) async {
