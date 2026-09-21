d76839726ef33f6276eace5168b3c9dc7e58fbb5
Kyrylo Stepanov <kstepanov@quantum-systems.com>
2026-09-21 14:57:18 +0300

    chore: updated CI toolkit & fixed tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gdf_hema_timer/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ScoreApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
